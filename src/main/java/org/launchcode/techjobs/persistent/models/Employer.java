3afaabf8c3c197665a554aa7c45d7ca3b9c70059
Masterlynx2 <Tonywatkins325@gmail.com>
2022-03-21 00:55:41 -0400

    for the love of god

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -21,10 +21,10 @@ public class Employer extends AbstractEntity {
 
     public Employer(){}
 
-    public Employer(String location){
-        super();
-        this.location = location;
-    }
+//    public Employer(String location){
+//        super();
+//        this.location = location;
+//    }
 
     public String getLocation() {
         return location;
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -22,10 +22,10 @@ public class Skill extends AbstractEntity {
 
     public Skill(){}
 
-    public Skill(String description){
-        super();
-        this.description=description;
-    }
+//    public Skill(String description){
+//        super();
+//        this.description=description;
+//    }
 
     public String getDescription() {
         return description;
